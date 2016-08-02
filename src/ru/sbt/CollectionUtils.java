966a9de32ebbdb1b56cbb803df2c3c4b7d69ba00
keyoh <kdiakov@yandex.ru>
2016-08-02 21:20:31 +0300

    1

--- src/ru/sbt/CollectionUtils.java
+++ src/ru/sbt/CollectionUtils.java
@@ -5,12 +5,11 @@ import java.util.List;
 
 public class CollectionUtils {
 
-    public static<T> void addAll(List<? extends T> source, List<? super T> destination) {
+    public static <T> void addAll(List<? extends T> source, List<? super T> destination) {
 
         destination.addAll(source);
 
     }
-
 
 
     public static <T> List<T> newArrayList() {
@@ -18,11 +17,9 @@ public class CollectionUtils {
     }
 
 
-
     public static <T> int indexOf(List<? extends T> source, T o) {
         return source.indexOf(o);
     }
-
 
 
     public static List limit(List source, int size) {
@@ -34,11 +31,9 @@ public class CollectionUtils {
     }
 
 
-
     public static void add(List source, Object o) {
         source.add(o);
     }
-
 
 
     public static void removeAll(List removeFrom, List c2) {
@@ -46,22 +41,20 @@ public class CollectionUtils {
     }
 
 
-
     public static <T> boolean containsAll(List<T> c1, List<T> c2) {
         for (T o : c2) {
             if (c1.contains(o)) {
                 return true;
+            } else {
+                return false;
             }
         }
-        return false;
     }
-
 
 
     public static boolean containsAny(List c1, List c2) {
 
     }
-
 
 
     public static List range(List list, Object min, Object max) {
