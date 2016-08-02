8872178c7ff6244a04f8721b701f14a848638f06
keyoh <kdiakov@yandex.ru>
2016-08-02 21:36:25 +0300

    1

--- src/ru/sbt/CollectionUtils.java
+++ src/ru/sbt/CollectionUtils.java
@@ -1,6 +1,7 @@
 package ru.sbt;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class CollectionUtils {
@@ -31,33 +32,44 @@ public class CollectionUtils {
     }
 
 
-    public static void add(List source, Object o) {
+    public static <T> void add(List<? super T> source, T o) {
         source.add(o);
     }
 
 
-    public static void removeAll(List removeFrom, List c2) {
+    public static <T> void removeAll(List<? super T> removeFrom, List<? extends T> c2) {
         removeFrom.removeAll(c2);
     }
 
-
+    //true если первый лист содержит все элементы второго
     public static <T> boolean containsAll(List<T> c1, List<T> c2) {
+        return c1.containsAll(c2);
+    }
+
+    //true если первый лист содержит хотя-бы 1 второго
+    public static <T> boolean containsAny(List<T> c1, List<T> c2) {
         for (T o : c2) {
             if (c1.contains(o)) {
                 return true;
-            } else {
-                return false;
             }
         }
+        return false;
     }
 
 
-    public static boolean containsAny(List c1, List c2) {
-
-    }
-
-
+    //Нужно создать копию листа, отсортировать его, и вернуть sublist, в котором находятся элементы в диапазоне от min до max.
+    // Элементы сравнивать через Comparable.
+    // Прмер range(Arrays.asList(8,1,3,5,6, 4), 3, 6) вернет {3,4,5,6}
     public static List range(List list, Object min, Object max) {
 
     }
+
+    //Нужно создать копию листа, отсортировать его, и вернуть sublist, в котором находятся элементы в диапазоне от min до max.
+    // Элементы сравнивать через Comparator.
+    // Прмер range(Arrays.asList(8,1,3,5,6, 4), 3, 6) вернет {3,4,5,6}
+    public static List range(List list, Object min, Object max, Comparator comparator) {
+
+    }
+
 }
+
